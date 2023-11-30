96347b8fa40a9b8206a8fedc388ab2d06a3ec94b
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-11-30 21:52:26 +0100

    Main z profili

---
+++ src/main/java/org/example/profiles/ExampleProfilesProperties.java
@@ -0,0 +1,11 @@
+package org.example.profiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleProfilesProperties {
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleProfilesProperties.class,args);
+    }
+}
