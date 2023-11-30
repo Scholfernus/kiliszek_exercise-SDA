0b4cb5cfd679703f4072247e0606c305a968d47a
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-11-30 21:54:15 +0100

    ProductionCommand - ustawienia produkcyjne

---
+++ src/main/java/org/example/profiles/command/ProductionCommand.java
@@ -0,0 +1,25 @@
+package org.example.profiles.command;
+
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Order(1)
+@Component
+@Profile("prod")
+public class ProductionCommand {
+
+    @Value("local properties")
+    private String overwritten;
+
+    @Value("not extended or extended")
+    private String extended;
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("LocalCommand {} {}",overwritten,extended);
+    }
+}
