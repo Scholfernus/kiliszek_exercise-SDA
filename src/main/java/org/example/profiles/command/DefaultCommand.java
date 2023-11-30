cb52e23853a80b268cbf801f14628f511e85a40b
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-11-30 21:52:59 +0100

    DefaultCommand - ustawienie domyślne

---
+++ src/main/java/org/example/profiles/command/DefaultCommand.java
@@ -0,0 +1,28 @@
+package org.example.profiles.command;
+
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Slf4j
+@Order(2)
+@Component
+public class DefaultCommand implements CommandLineRunner {
+
+    @Value("${java.org.example.profiles.command.overwritten}")
+    private String overwritten;
+    @Value("${org.example.profiles.command.extended}")
+    private String extended;
+
+    //Dodatkowe możliwości @Value
+    @Value("#{'${spring.profiles.active:unknown}'.split(',')}")
+    public List<String>activeProfile;
+    @Override
+    public void run(String... args) throws Exception {
+    log.info("Default command {} {} {}", overwritten, extended, activeProfile);
+    }
+}
